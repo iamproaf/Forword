491e301aaa28203ceec1d1f3e116a23f17603574
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-04-15 00:41:55 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,6 +8,4 @@ class Translation(object):
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
 <b>👨‍💻 Developer:</b> @lx0980AI
-<b>📢 Channel:</b> @DFF_UPDATES
-<b>👥 Group:</b> @Hollywood_0980
 <b>🌐Source Code:</b> https://github.com/0AIB/Auto-Forward-Bot"""
